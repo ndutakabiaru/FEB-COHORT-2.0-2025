9843d0f8bf3418a7eb9536550823dffd3b3bdf10
ndutakabiaru <kabiarunduta@gmail.com>
2025-03-11 11:13:15 +0300

    add message

---
+++ PythonNavigation/intro2.py
@@ -0,0 +1,20 @@
+#integer (Whole number)
+age = 25
+#float (decimal numbers)
+height = 5.9
+#string (text)
+name = " Nduta "
+#boolean (True/False)
+is_student = True
+
+#Check data types
+print (type(age))  #Output:<class 'int'>
+print (type(height))  #Output:<class 'int'>
+print (type(name))  #Output:<class 'int'>
+print (type(is_student))  #Output:<class 'int'>
+
+#Dynamic typing:
+age = 20
+print(age) #output : 20
+age = "Now I'm twenty"
+print (age) # output : string
