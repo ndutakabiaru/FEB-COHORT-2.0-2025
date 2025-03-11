67600938a9626dc50b5c83a5dba63f69e81cf25a
ndutakabiaru <kabiarunduta@gmail.com>
2025-03-11 11:41:09 +0300

    new commit

--- PythonNavigation/week 1/day 1/index.py
+++ PythonNavigation/week 1/day 1/index.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+number = 10
+print(number)
+
+age = 20
+marks = 98
+
+name = "Nduta Kabiaru"
+#Lists Datatypes
+
+languages = ["python","html","css",3,5.7,True]
+print(languages)
+print(languages[2])
